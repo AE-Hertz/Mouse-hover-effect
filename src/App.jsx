47b34d68b48a1919efe32ff3da4ee0819bc18b9f
Abhi-hertz <93651229+AE-Hertz@users.noreply.github.com>
2024-10-06 13:05:56 +0530

    init v2 (component built completely)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
 import Button from "./component/Button"
 import img from "./assets/mygirl.jpg"
 
 const App = () => {
   return (
     <div className="button-group">
-      {/* <Button type="homepage">Homepage</Button>
-      <Button type="discover-more">Discover more</Button> */}
+      <Button type="homepage">
+        <span>Homepage</span>
+      </Button>
+      <Button type="discover-more">
+        <span>Discover more</span>
+      </Button>
       <Button type="get-in-touch" imgSrc={img}>
         <span>Get in touch</span>
       </Button>
